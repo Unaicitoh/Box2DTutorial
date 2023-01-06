657adfaaa287a69c8501ac00bcf5ba2268ffe6af
Unai Gonzalez Sanchez <unaigsanchez@gmail.com>
2023-01-06 23:33:46 +0100

    Test

---
+++ desktop/src/com/gamesugs/box2dtutorial/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.gamesugs.box2dtutorial;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.gamesugs.box2dtutorial.Box2DTutorial;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Box2DTutorial");
+		new Lwjgl3Application(new Box2DTutorial(), config);
+	}
+}
---
+++ ios/src/com/gamesugs/box2dtutorial/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.gamesugs.box2dtutorial;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.gamesugs.box2dtutorial.Box2DTutorial;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Box2DTutorial(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
